6fa04db9ab3c5b6b44d7565f938b997e8ca21f79
unknown <Administrator@QCL-00330.h2o.com>
2021-11-12 16:13:33 -0500

    ''

--- myContainer/settings.py
+++ myContainer/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calm-fortress-90737.herokuapp.com','127.0.0.1']
 
 
 # Application definition
